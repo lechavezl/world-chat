7431ec52863220a2bd63b74fead29aa79c296ae1
lechavezl <luisernestochavezleal@gmail.com>
2024-09-10 16:33:36 -0400

    Starter code for the proyect

--- server.js
+++ server.js
@@ -6,15 +6,26 @@ const app = express()
 const env = require("dotenv").config()
 const expressLayouts = require("express-ejs-layouts")
 const bodyParser = require("body-parser")
+const static = require("./routes/static")
 const HOST = 'localhost'
 const PORT = 3000
 
 /* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
  * Routes
  *************************/
+app.use(static)
 
-app.get("/", (req, res) => {res.send("Welcome home!")})
-
+// Log in route - Main Route
+app.get("/", (req, res) => {
+    res.render("index", {title: "Log In | World Chat"})
+})
 
 
 
